b829e93fe22bd45b54d08b7e63482470c5f8ef21
Vítor Marco <vitoradrianomarco@gmail.com>
2023-07-23 11:58:15 -0300

    feat: create border, breakponts and color

---
+++ src/styles/theme/color.ts
@@ -0,0 +1,48 @@
+const color = {
+  primary: {
+    lightest: 'hsl(60, 80%, 90%)',
+    light: 'hsl(60, 80%, 70%)',
+    base: 'hsl(60, 80%, 50%)',
+    dark: 'hsl(60, 80%, 30%)',
+    darkest: 'hsl(60, 80%, 10%)',
+  },
+  secondary: {
+    lightest: 'hsl(240, 80%, 90%)',
+    light: 'hsl(240, 80%, 70%)',
+    base: 'hsl(240, 80%, 50%)',
+    dark: 'hsl(240, 80%, 30%)',
+    darkest: 'hsl(240, 80%, 10%)',
+  },
+  neutral: {
+    white: 'hsl(0, 0%, 100%)',
+    lightest: 'hsl(0, 0%, 95%)',
+    light: 'hsl(0, 0%, 80%)',
+    base: 'hsl(0, 0%, 50%)',
+    dark: 'hsl(0, 0%, 20%)',
+    black: 'hsl(0, 0%, 0%)',
+  },
+  positive: {
+    base: 'hsl(120, 80%, 50%)',
+  },
+  negative: {
+    base: 'hsl(0, 80%, 50%)',
+  },
+  opacity: {
+    black: {
+      ten: 'hsl(0, 0%, 0%, 1)',
+      eight: 'hsl(0, 0%, 0%, 0.8)',
+      six: 'hsl(0, 0%, 0%, 0.6)',
+      four: 'hsl(0, 0%, 0%, 0.4)',
+      one: 'hsl(0, 0%, 0%, 0.1)',
+    },
+    white: {
+      ten: 'hsl(0, 0%, 100%, 1)',
+      eight: 'hsl(0, 0%, 100%, 0.8)',
+      six: 'hsl(0, 0%, 100%, 0.6)',
+      four: 'hsl(0, 0%, 100%, 0.4)',
+      one: 'hsl(0, 0%, 100%, 0.1)',
+    },
+  },
+}
+
+export default color
---
+++ src/styles/theme/index.ts
@@ -0,0 +1,11 @@
+import border from './border'
+import breakpoints from './breakpoint'
+import color from './color'
+
+const theme = {
+  border,
+  breakpoints,
+  color,
+}
+
+export default theme
