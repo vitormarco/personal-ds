fc3e4e867cd304e9a7975a05ca6571fae8c34995
Vítor Marco <vitoradrianomarco@gmail.com>
2023-07-29 18:08:03 -0300

    feat: add test for progress bar and visually hidden

--- src/utils/sum.ts
+++
@@ -1,4 +0,0 @@
-const sum = (...numbers: number[]) =>
-  numbers.reduce((total, number) => total + number, 0)
-
-export default sum
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,19 @@
 import path, { resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(dirname, './src') }],
   },
   test: {
     environment: 'jsdom',
+    globals: true,
+    setupFiles: './test.config.ts',
   },
 })
